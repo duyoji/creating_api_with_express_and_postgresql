b7f9369af88a4ec14d6ccfca4e3dd3ad92d2f38e
haruka-g <mortndpgj@gmail.com>
2019-12-22 21:12:43 +0900

    seedersファイルを作成、実行

---
+++ src/db/seeders/20191222114150-demo-todo.js
@@ -0,0 +1,36 @@
+"use strict";
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    const todos = [1, 2, 3, 4, 5].map(item => {
+      return {
+        title: "title" + item,
+        body: "body" + item,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      };
+    });
+    return queryInterface.bulkInsert("Todos", todos, {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+    return queryInterface.bulkDelete("Todos", null, {});
+  }
+};
