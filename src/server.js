3056089b11b23886815b5528de2583faa68a78e5
haruka-g <mortndpgj@gmail.com>
2019-12-31 00:39:08 +0900

    morganをインストール、server.jsに実装

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const app = express();
 const router = require('./routes/api.router');
+const logger = require('morgan');
+const app = express();
 
+app.use(logger());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
