49821dd458a0e2a52eb8671aeb564338e2dec347
haruka-g <mortndpgj@gmail.com>
2020-01-04 22:00:25 +0900

    GET api/todosのテスト実装

--- test/app/api/todos/getTodos.test.js
+++ test/app/api/todos/getTodos.test.js
@@ -1,4 +1,5 @@
 const helper = require('../../../helper/requestHelper');
+const assert = require('power-assert');
 
 describe('test GET /api/todos', () => {
   it('レスポンスのテスト', async () => {
@@ -7,6 +8,18 @@ describe('test GET /api/todos', () => {
       endPoint: '/api/todos',
       statusCode: 200,
     });
-    console.log(response.body);
+
+    const todos = response.body;
+
+    assert.strictEqual(Array.isArray(todos), true);
+    assert.strictEqual(todos.length, 5);
+
+    todos.forEach(todo => {
+      assert.strictEqual(typeof todo.id, 'number');
+      assert.strictEqual(typeof todo.title, 'string');
+      assert.strictEqual(typeof todo.body, 'string');
+      //Mysqlにbooleanという型はない。0と1で表現されている。https://qiita.com/ritukiii/items/3a3667391d4d65678d82
+      assert.strictEqual(typeof todo.completed, 'number');
+    });
   });
 });
