199e5d8d56b9222b7d27d28e7b98f95bcbc66130
haruka-g <mortndpgj@gmail.com>
2020-01-13 10:23:49 +0900

    api.controller.jsの実装

--- src/controllers/api.controller.js
+++ src/controllers/api.controller.js
@@ -28,8 +28,45 @@ const todosController = {
       res.status(400).json({ message: error.message });
     }
   },
-  putTodo(req, res) {
-    res.status(200).json({ message: 'putTodo', id: req.params.id });
+  async putTodo(req, res) {
+    let transaction;
+    try {
+      const { title, body, completed } = await req.body;
+      const { id } = req.params;
+      const parsedId = parseInt(id, 10);
+
+      if (parsedId < 1 || isNaN(parsedId)) {
+        const error = new Error('IDは必須です(1以上の数値)');
+        throw error;
+      }
+
+      transaction = await db.sequelize.transaction();
+
+      //https://sequelize.org/master/manual/models-usage.html#-code-find--code----search-for-one-specific-element-in-the-database
+      const targetTodo = await db.Todo.findByPk(parsedId, { transaction });
+      if (!targetTodo) {
+        const error = new Error('存在しないIDです');
+        error.status = 404;
+        throw error;
+      }
+
+      const updatedTodo = await targetTodo.update(
+        {
+          id: parsedId,
+          title,
+          body,
+          completed,
+        },
+        { transaction }
+      );
+      await transaction.commit();
+      res.status(200).json({ updatedTodo });
+    } catch (error) {
+      res.status(error.status || 400).json({ message: error.message });
+      //line38~41で投げられたエラーを返せるようにrollbackは最後に行うように修正。
+      //上記のエラーはtransaction経由で投げられていないため。
+      await transaction.rollback();
+    }
   },
   deleteTodo(req, res) {
     res.status(200).json({ message: 'deleteTodo', id: req.params.id });
